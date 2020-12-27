311f7b2266c9b12988688ee5fd5c675ba58c7fbf
Celeo <mattboulanger@fastmail.com>
2020-12-26 19:34:02 -0800

    Update to React 17
    Mostly

--- tests/setupTests.ts
+++ tests/setupTests.ts
@@ -1,4 +1,8 @@
 import Enzyme from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
+
+// TODO fix when enzyme releases support for React 17
+// https://github.com/enzymejs/enzyme/issues/2429
+// import Adapter from "enzyme-adapter-react-17";
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
 
 Enzyme.configure({ adapter: new Adapter() });
--- tests/styleMock.ts
+++ tests/styleMock.ts
@@ -1,1 +1,3 @@
-export default {};
+const mock = {};
+
+export default mock;
